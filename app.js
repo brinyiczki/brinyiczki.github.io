db39ad4cd22d3bb67e90f3d13a03466958dad83c
Brinyiczki <103284604+brinyiczki@users.noreply.github.com>
2024-01-21 17:41:04 +0100

    yes

--- app.js
+++
@@ -1,5 +0,0 @@
-import {MDCRipple} from '@material/ripple';
-import {MDCTextField} from '@material/textfield';
-
-new MDCTextField(document.querySelector('.food-field'));
-new MDCRipple(document.querySelector('.eat-button'));
